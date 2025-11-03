66abcb5caa0082683bd262d021e900653429cdd6
Anna Dao <anna.dao@gmx.net>
2025-11-03 21:20:15 +0100

    Experimentieren in vue

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,11 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="PeriodenTracker" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Menü</RouterLink>
+        <RouterLink to="/about">Kalender</RouterLink>
       </nav>
     </div>
   </header>
@@ -43,7 +43,7 @@ nav a.router-link-exact-active {
 }
 
 nav a.router-link-exact-active:hover {
-  background-color: transparent;
+  background-color: pink;
 }
 
 nav a {
