3790953c32300297752b06d10c1bea7de655cef7
Anna Dao <anna.dao@gmx.net>
2025-11-06 16:23:29 +0100

    in HelloWorld "häufige Symptome" + Umahmung & neue vue Kalender

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 defineProps<{
   msg: string
 }>()
+
+type Symptom = { id: number; name: string }
+
+const symptoms = ref<Symptom[]>([
+  { id: 1, name: 'Krämpfe' },
+  { id: 2, name: 'Kopfschmerzen' },
+  { id: 3, name: 'Müdigkeit' },
+  { id: 4, name: ' Stimmungsschwankungen'}
+])
 </script>
 
 <template>
@@ -12,6 +23,16 @@ defineProps<{
       <!--a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a-->
       <!--a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a-->
     </h3>
+
+    <!-- Symptomliste -->
+    <section class = "card">
+      <h4> Häufige Symptome </h4>
+      <ul>
+        <li v-for = "s in symptoms" :key = "s.id">
+          {{s.name}}
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
@@ -31,6 +52,15 @@ h3 {
 .greetings h1,
 .greetings h3 {
   text-align: left;
+}
+
+/* Umrahmung bei Symptoms
+ */
+.card {
+  margin-top: 1rem;
+  padding: 1rem;
+  border: 1px solid var(--color-border);
+  border-radius: 10px;
 }
 
 @media (min-width: 1024px) {
---
+++ src/components/Kalendar.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { computed, toRefs } from 'vue'
+
+type Props = {
+  year?: number
+  month?: number // 0-11 (JS-Standard). 0=Jan, 9=Okt
+}
+const props = defineProps<Props>()
+const { year, month } = toRefs(props)
+
+// Fallback: aktueller Monat
+const base = computed(() => {
+  const now = new Date()
+  return {
+    y: year?.value ?? now.getFullYear(),
+    m: month?.value ?? now.getMonth()
+  }
+})
+
+// Anzahl Tage
+const daysInMonth = computed(() => {
+  return new Date(base.value.y, base.value.m + 1, 0).getDate()
+  }
+)
+
+// Array [1..N] für v-for
+const days = computed(() =>
+  Array.from({ length: daysInMonth.value }, (_, i) => i + 1)
+)
+
+// Auswahl nach oben melden
+const emit = defineEmits<{ (e: 'select', payload: { day: number, year: number, month: number }): void }>()
+function selectDay(day: number) {
+  emit('select', { day, year: base.value.y, month: base.value.m })
+}
+</script>
+
+
+<template>
+  <div class = "grid">
+    <button
+      v-for = "d in days"
+      :key = "d"
+      class = "cell"
+      type = "button"
+      @click = "selectDay(d)"
+      :aria-label = "`Tag ${d}`"
+    >
+      {{ d }}
+    </button>
+  </div>
+</template>
+
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  gap: .5rem;
+  padding: .5rem;
+}
+.cell {
+  aspect-ratio: 1 / 1;
+  border: 1px solid var(--color-border);
+  border-radius: .5rem;
+  background: var(--color-background);
+  cursor: pointer;
+  font: inherit;
+}
+.cell:hover {
+  outline: 2px solid pink;
+}
+</style>
